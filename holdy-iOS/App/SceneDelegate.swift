0f8f38a16c7b8b6d191a7ed70f80746e9a19e748
yanghojoon <yanghojoon8487@gmail.com>
2022-07-30 20:31:16 +0900

    chore: 프로젝트 초기 셋팅
    - iOS Deployment Target 15.0으로 설정
    - 스토리보드 제거
    - 아이패드의 경우 지원 X

---
+++ holdy-iOS/App/SceneDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SceneDelegate.swift
+//  holdy-iOS
+//
+//  Created by 양호준 on 2022/07/30.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let navigationController = UINavigationController()
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
+    }
+
+    // MARK: - Scene Lifecycle
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
+
