a6d6f8c8cc99a9852bfe9506de64f573bde32938
ellenJ <jcrescent61@gmail.com>
2022-08-14 23:59:46 +0900

    docs: GroupListViewController 파일 생성

---
+++ holdy-iOS/Presentation/Home/View/GroupListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GroupListViewController.swift
+//  holdy-iOS
+//
+//  Created by Ellen on 2022/08/14.
+//
+
+import UIKit
+
+final class GroupListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
