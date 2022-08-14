8506b5934b4ac30fba6800d5c230f4128d4a57e2
ellenJ <jcrescent61@gmail.com>
2022-08-15 02:24:40 +0900

    feat: BaseRouter requestGroupList 메소드 구현
    - API 서버에서 모임 리스트를 요청하고 Single<Decodable>로 반환하는 메소드

--- holdy-iOS/Data/RemoteDB/BaseRouter.swift
+++ holdy-iOS/Data/RemoteDB/BaseRouter.swift
@@ -83,4 +83,26 @@ struct BaseRouter {
             return Disposables.create()
         }
     }
+    
+    func requestGroupList<T: Decodable>(api: Gettable, decodingType: T.Type) -> Single<T> {
+            return Single.create { element in
+                let loginSession: String? =  UserDefaults.standard.string(forKey: "loginSession")
+                let headers: HTTPHeaders = ["Cookie": loginSession ?? ""]
+                let httpMethod = HTTPMethod(rawValue: api.method.description)
+
+                AF.request(api.url ?? URL(fileURLWithPath: ""),
+                           method: httpMethod,
+                           headers: headers)
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: T.self) { response in
+                    switch response.result {
+                    case .success(let data):
+                        element(.success(data))
+                    case .failure(let error):
+                        element(.failure(error))
+                    }
+                }
+                return Disposables.create()
+            }
+        }
 }
