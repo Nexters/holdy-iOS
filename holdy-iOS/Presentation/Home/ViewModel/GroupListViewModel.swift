fd4146e30455cbcd2fa7878bee856038cd59cb18
양호준 <yanghojoon8487@gmail.com>
2022-10-02 15:12:42 +0900

    [#11] Task01/hide complete group (#12)
    * refactor: 새로운 모임이 추가되는 경우 가장 상단으로 스크롤되도록 수정
    * feat: 완료된 모임을 숨기는 버튼 액션 구현

--- holdy-iOS/Presentation/Home/ViewModel/GroupListViewModel.swift
+++ holdy-iOS/Presentation/Home/ViewModel/GroupListViewModel.swift
@@ -10,17 +10,18 @@ import RxSwift
 
 final class GroupListViewModel {
     struct Input {
-        let viewWillAppear: Observable<Bool>
+        let loadDataWithViewWillAppear: Observable<Bool>
     }
     
     struct Output {
         let groupInfos: Observable<[GroupInfo]>
     }
     
+    private(set) var isFiltered = false
     private let router = GroupListRouter()
     
     func transform(_ input: Input) -> Output {
-        let groupInfos = configureGroupInfos(with: input.viewWillAppear)
+        let groupInfos = configureGroupInfos(with: input.loadDataWithViewWillAppear)
         let output = Output(
             groupInfos: groupInfos
         )
@@ -31,7 +32,11 @@ final class GroupListViewModel {
     private func configureGroupInfos(with inputObserver: Observable<Bool>) -> Observable<[GroupInfo]> {
         inputObserver
             .withUnretained(self)
-            .flatMap { (viewModel, _) -> Single<GroupInfoResponse> in
+            .flatMap { (viewModel, isViewWillAppearCalled) -> Single<GroupInfoResponse> in
+                if !isViewWillAppearCalled {
+                    viewModel.isFiltered.toggle()
+                }
+                
                 let response = viewModel.router.requestGroupList(
                     api: HoldyAPI.GetGroupList(),
                     decodingType: GroupInfoResponse.self
@@ -39,7 +44,14 @@ final class GroupListViewModel {
                 
                 return response
             }
-            .map { groupInfoResponse in
+            .withUnretained(self)
+            .map { viewModel, groupInfoResponse in
+                if viewModel.isFiltered {
+                    return groupInfoResponse.data.filter { groupInfo in
+                        groupInfo.isEnd == false
+                    }
+                }
+                
                 return groupInfoResponse.data
             }
     }
