0c771410a8c25e91f67eeac3dae59f45218c4c82
yanghojoon <yanghojoon8487@gmail.com>
2022-10-03 19:46:10 +0900

    feat: 초대를 받는 경우 리스트에 뜰 수 있도록 구현
    - 서버에 모임 아이디로 요청

--- holdy-iOS/App/SceneDelegate.swift
+++ holdy-iOS/App/SceneDelegate.swift
@@ -4,9 +4,13 @@
 
 import UIKit
 
+import RxSwift
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     var appCoordinator: AppCoordinator?
+    private let router = InvitationpRouter()
+    private let disposeBag = DisposeBag()
 
     func scene(
         _ scene: UIScene,
@@ -38,8 +42,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // MARK: - Kakao Link
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         if let url = URLContexts.first?.url {
-            // TODO: 나중에 링크 연결할 수 있도록 수정
-            print(url)
+            let query = url.query
+            if let range = query?.range(of: "="), var query = query {
+                query.removeSubrange(query.startIndex...range.lowerBound)
+                
+                guard let id = Int(query) else { return }
+                
+                let response = router.reqeustInvitation(
+                    api: HoldyAPI.RequestInvitaion(),
+                    id: id,
+                    decodingType: InvitaionResponse.self
+                )
+                
+                response.asObservable()
+                    .subscribe(onNext: { response in
+                        #if DEBUG
+                        print(response)
+                        #endif
+                    })
+                    .disposed(by: disposeBag)
+            }
         }
     }
 }
